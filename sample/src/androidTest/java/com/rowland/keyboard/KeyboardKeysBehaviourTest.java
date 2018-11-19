66d48d6698cd30b5c6d6c3f861bdb4545454f60e
RowlandOti <rowlandotienoo@gmail.com>
2018-11-19 15:49:03 +0300

    tests: check that backspace and space function as expected. This will test effect of both click() and longclick().

---
+++ sample/src/androidTest/java/com/rowland/keyboard/KeyboardKeysBehaviourTest.java
@@ -0,0 +1,56 @@
+package com.rowland.keyboard;
+
+import com.rowland.keyboard.sample.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class KeyboardKeysBehaviourTest {
+
+    @Rule
+    public ActivityTestRule<KeyboardActivity> mActivityRule = new ActivityTestRule<>(KeyboardActivity.class);
+    private String mStringToBetyped;
+
+    @Before
+    public void initValidString() {
+        mStringToBetyped = "QWERTY";
+    }
+
+    @Test
+    public void testSpaceAndBackSpaceKeys() {
+        // Type text and then press the button.
+        onView(withId(R.id.input_text)).perform(typeText(mStringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.input_text)).check(matches(not(withText(mStringToBetyped))));
+
+        onView(withId(R.id.key_backspace)).perform(click());
+        // Check that the text was changed.
+        onView(withId(R.id.input_text)).check(matches(not(withText(mStringToBetyped))));
+
+
+        onView(allOf(withId(R.id.key_backspace))).check(matches(isDisplayed())).perform(longClick());
+        // Check that the text was deleted
+        onView(withId(R.id.input_text)).check(matches(not(withText(""))));
+
+        onView(allOf(withId(R.id.key_space))).check(matches(isDisplayed())).perform(click());
+        // Check that the text was deleted
+        onView(withId(R.id.input_text)).check(matches(not(withText(" "))));
+    }
+}
