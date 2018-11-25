876a86aecdc0636f0ea6f3b3a8a4020f934cc699
RowlandOti <rowlandotienoo@gmail.com>
2018-11-25 08:07:05 +0300

    refactor: just some code refactor.

--- kokokeyboard/src/main/java/com/rowland/kokokeyboard/keys/SquareKeyView.java
+++ kokokeyboard/src/main/java/com/rowland/kokokeyboard/keys/SquareKeyView.java
@@ -16,14 +16,20 @@ public class SquareKeyView extends AppCompatButton {
 
     public SquareKeyView(Context context) {
         super(context);
+        init(context, null, 0);
     }
 
     public SquareKeyView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context, attrs, 0);
     }
 
     public SquareKeyView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.KeyPadView);
         try {
             this.screenWidthPercentage = a.getFloat(R.styleable.KeyPadView_screen_width_percent, 0.20f);
@@ -33,20 +39,31 @@ public class SquareKeyView extends AppCompatButton {
         }
     }
 
+
     @Override
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        /*int minW = getPaddingLeft() + getPaddingRight() + getSuggestedMinimumWidth();
+       /*int minW = getSuggestedMinimumWidth();
         int measuredWidth = getDefaultSize(minW, widthMeasureSpec);
 
-        int minH = getPaddingBottom() + getPaddingTop() + getSuggestedMinimumHeight();
+        int minH =  getSuggestedMinimumHeight();
         int measuredHeight = getDefaultSize(minH, heightMeasureSpec);
 
         int finalWidth = measuredWidth;
         int finalHeight = (int) (measuredWidth / widthHeightRatio);
 
-        setMeasuredDimension(finalWidth, finalHeight);*/
+        setMeasuredDimension(finalWidth, finalHeight);
+*/
+        /*if (widthHeightRatio > 0.0) {
+            // set the image views size
+            int width = MeasureSpec.getSize(widthMeasureSpec);
+            int height = (int) (width * widthHeightRatio);
+            setMeasuredDimension(width, height);
+        }
+        else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }*/
     }
 
 
